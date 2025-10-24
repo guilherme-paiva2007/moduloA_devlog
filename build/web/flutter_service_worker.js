d809512e0a6be51768b98480bcad5eba04763817
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-10-24 16:20:37 -0300

    build_web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b3370a27a4d1e42f852146052d78c512
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "2d9f8aa935e2774f2806e64f3f99bad9",
+"flutter_bootstrap.js": "fddf67267e45e6bfa18cb3879a39922d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
